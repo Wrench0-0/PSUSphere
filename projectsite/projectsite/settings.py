c4eca7b17f05e4a35ede872089021eeb5a933c78
Wrench0-0 <202380356@psu.palawan.edu.ph>
2026-02-27 23:53:57 +0800

    added pythonanywhere

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-!bhn(ee#00&8@g!(=cfasog7@@&3fde&qs!!xo9t#cewjpsrzp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'wrenchie0.pythonanywhere.com',
+    'psusphere.pythonanywhere.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
