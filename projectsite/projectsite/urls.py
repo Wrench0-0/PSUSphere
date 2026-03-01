80db230f948b37bc30f01c805dd962dbae2032f4
n3ck-hurt <202380085@psu.palawan.edu.ph>
2026-03-01 20:31:50 +0800

    part 4 psusphere

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView
+
+from studentorg.views import (
+    HomePageView,
+    OrganizationList,
+    OrganizationCreateView,
+    OrganizationUpdateView,
+    OrganizationDeleteView,
+)
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('admin/', admin.site.urls),
+    path("", HomePageView.as_view(), name="home"),
+    path("admin/", admin.site.urls),
+    path("organization_list/", OrganizationList.as_view(), name="organization-list"),
+    path(
+        "organization_list/add/",
+        OrganizationCreateView.as_view(),
+        name="organization-add",
+    ),
+    path(
+        "organization_list/<int:pk>/",
+        OrganizationUpdateView.as_view(),
+        name="organization-update",
+    ),
+    path(
+        "organization_list/<int:pk>/delete/",
+        OrganizationDeleteView.as_view(),
+        name="organization-delete",
+    ),
 ]
